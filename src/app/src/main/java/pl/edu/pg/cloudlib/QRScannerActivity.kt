f9e912a478ac566d53f7e9d043b7047158ce4a42
Wiktor Dąbrowski <dabrowski.wiktor.01@gmail.com>
2023-06-26 00:32:04 +0200

    Added Firebase connection
    Can read/write const messages on create QR Scanner Activity

--- src/app/src/main/java/pl/edu/pg/cloudlib/QRScannerActivity.kt
+++ src/app/src/main/java/pl/edu/pg/cloudlib/QRScannerActivity.kt
@@ -14,6 +14,8 @@ import com.budiyev.android.codescanner.CodeScanner
 import com.budiyev.android.codescanner.DecodeCallback
 import com.budiyev.android.codescanner.ErrorCallback
 import com.budiyev.android.codescanner.ScanMode
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.zxing.BarcodeFormat
 import pl.edu.pg.cloudlib.databinding.ActivityQrscannerBinding
 
@@ -54,7 +56,53 @@ class QRScannerActivity : AppCompatActivity() {
             codeScanner.startPreview()
         }
 
+        val db = Firebase.firestore
 
+        // Create a new user with a first and last name
+        var user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+        // Create a new user with a first, middle, and last name
+        user = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
 
     private fun getPermission(permission: String){
