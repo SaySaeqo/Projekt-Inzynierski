f52872a320459ff72fdb75c0990e1f21831a7dba
saysaeqo <saysaeqo@gmail.com>
2023-12-09 12:00:54 +0100

    Partially mechanism for links in exhibit extra (new data structure needed)

--- editor_frontend/src/components/EditorExtra.vue
+++ editor_frontend/src/components/EditorExtra.vue
@@ -3,15 +3,24 @@
         <div class="item" v-for="x in extra" :key="x[0]">
         <p>{{ x[0] }}</p>
         <input type="text" v-model="x[1]" />
+        <select v-model="selectedExhibit">
+          <option v-for="exhibit in exhibits" :key="exhibit.id" :value="exhibit">
+            {{ exhibit.name }}
+          </option>
+        </select>
         <div>
             <button @click="saveExtras(x[0], x[1])">SAVE</button>
-
             <button @click="removeExtras(x[0])">REMOVE</button>
         </div>
         </div>
         <div class="item">
             <input type="text" v-model="extra_key" />
             <input type="text" v-model="extra_value" />
+            <select v-model="selectedExhibit">
+              <option v-for="exhibit in exhibits" :key="exhibit.id" :value="exhibit">
+                {{ exhibit.name }}
+              </option>
+            </select>
             <button class="add" @click="addExtras">ADD</button>
         </div>
         <p>{{ error_msg }}</p>
@@ -21,6 +30,8 @@
   <script lang="ts">
   import { defineComponent, PropType } from "vue";
   import BaseWidget from "./BaseWidget.vue";
+  import dataService from "../services/DataService";
+import { Exhibit } from "@/models/Exhibit";
   
   export default defineComponent({
     props: {
@@ -37,14 +48,23 @@
         extra_key: "",
         extra_value: "",
         error_msg: "",
+        exhibits: [] as Exhibit[],
+        selectedExhibit: "",
       };
     },
+    created() {
+      this.getExhibit();
+    },
     methods: {
+      async getExhibit() {
+        this.exhibits = await dataService.getAll();
+      },
       addExtras() {
         if (this.extra_key.length > 0 && this.extra_value.length > 0) {
           this.extra.set(this.extra_key, this.extra_value);
           this.extra_key = "";
           this.extra_value = "";
+          this.selectedExhibit = "";
           this.error_msg = "";
         } else {
           this.error_msg = "Please fill in both fields";
@@ -71,7 +91,15 @@
       gap: 1em;
       align-items: center;
       display: grid;
-      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-columns: 1fr 1fr 1fr 2fr;
+      max-width: 30em;
+
+      input {
+        max-width: 8em;
+      }
+      select {
+        max-width: 10em;
+      }
     }
   
     .add {
